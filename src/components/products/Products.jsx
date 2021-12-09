a59aa2453015f4b5315db2365aa1ab6f9ad63adb
milad heidari <milad.heidari.dev@gmail.com>
2021-12-09 11:59:33 +0330

    import connect form react-redux and send getProduct dispatch for get products data

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,16 +1,30 @@
 import React,{useEffect} from 'react'
+import {connect} from 'react-redux'
+import actions from '../../store/actions'
 
-function Products() {
+function Products({getProducts}) {
 
     useEffect(() => {
-
-    }, [])
+        getProducts({url:'products'})
+    },[])
 
     return (
         <div>
-            
+            products components
         </div>
     )
 }
 
-export default Products
+const mapDispatch = (dispatch)=>{
+    return {
+        getProducts:(payload)=>{
+            dispatch({
+                type:actions.GET_PRODUCTS,
+                payload
+            })
+        }
+    }
+}
+
+
+export default connect(null,mapDispatch)(Products)

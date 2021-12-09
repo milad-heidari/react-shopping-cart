2b041d3180d41d0bbd2518f2ad8a9b504bae5349
milad heidari <milad.heidari.dev@gmail.com>
2021-12-09 11:56:26 +0330

    import and use Products components in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Nav from "./components/nav/Nav";
+import Products from './components/products/Products';
 
 function App() {
   return (
     <>
       <Nav/>
+      <Products/>
     </>
   );
 }

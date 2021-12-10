d736fe5343af7ecc27f3f9585643017bb524fcb8
milad heidari <milad.heidari.dev@gmail.com>
2021-12-10 17:13:50 +0330

    conncet nav component to state and handle cart visibility btn

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import {connect} from 'react-redux'
+import actions from "../../store/actions";
 import './Nav.css'
-function Nav() {
+function Nav({showCart}) {
+
+  const showCartHandler = ()=>{
+    showCart()
+  }
+
   return (
     <nav className="nav">
       <div className="nav__container">
         <h3 className="nav__container-title">shoping</h3>
-        <span className="nav__container-cart-icon">
+        <span onClick={showCartHandler} className="nav__container-cart-icon">
           <i className="fas fa-cart-plus"></i>
           <span className="nav__container-cart-badge">0</span>
         </span>
@@ -14,4 +21,14 @@ function Nav() {
   );
 }
 
-export default Nav;
+const mapDispatch = (dispatch)=>{
+  return {
+    showCart:()=>{
+      dispatch({
+        type:actions.CHANGE_CART_VISIBILITY,
+      })
+    }
+  }
+}
+
+export default connect(null,mapDispatch)(Nav);

658510a3502ddb9a3a6b6f2b8b3c78dcf80a90bc
milad heidari <milad.heidari.dev@gmail.com>
2021-12-10 17:17:07 +0330

    connect App.jsx to state and render Modal component with conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import Modal from './components/modal/Modal';
 import Nav from "./components/nav/Nav";
 import Products from './components/products/Products';
 
-function App() {
+function App({cartVisibility}) {
+  
   return (
     <div className='App'>
       <Nav/>
+      {cartVisibility ? <Modal/>:null}
       <Products/>
     </div>
   );
 }
 
-export default App;
+const mapState = (state)=>{
+  return {
+    cartVisibility:state.cartVisibility
+  }
+}
+
+export default connect(mapState)(App);

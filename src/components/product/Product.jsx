fb584ca1ebd78f02a66deef44784bb810fd3445d
milad heidari <milad.heidari.dev@gmail.com>
2021-12-10 16:43:34 +0330

    get add product props and add to cart

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,14 +1,32 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import actions from '../../store/actions'
 import './Product.css'
-function Product({title,image,price}) {
+function Product({product,addToCart}) {
+
+    const addToCartHandler = ()=>{
+      addToCart({...product})
+    }
+
     return (
         <div className="product-container">
-          <img src={image} alt="pic" />
-          <p className="product-container__title">{title}</p>
-          <p>{`$${price}`}</p>
-          <button>add to cart</button>
+          <img src={product.image} alt="pic" />
+          <p className="product-container__title">{product.title}</p>
+          <p>{`$${product.price}`}</p>
+          <button onClick={addToCartHandler} >add to cart</button>
         </div>
     )
 }
 
-export default Product
+const mapDispatch = (dispatch)=>{
+  return {
+    addToCart:(payload)=>{
+      dispatch({
+        type:actions.ADD_TO_CART,
+        payload
+      })
+    }
+  }
+}
+
+export default connect(null,mapDispatch)(Product)

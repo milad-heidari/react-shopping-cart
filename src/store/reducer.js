829c1b86b3d44058c0c1779c8f66e03733555ed6
milad heidari <milad.heidari.dev@gmail.com>
2021-12-10 17:07:26 +0330

    add cartVisibility to state

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,6 +2,7 @@ import actions from './actions'
 const initState = {
     products:[],
     cart:[],
+    cartVisibility:false
 }
 
 const reducer = (state=initState,action)=>{
@@ -17,6 +18,13 @@ const reducer = (state=initState,action)=>{
             break;
         case actions.ADD_TO_CART:
             newState= {...state,cart:[...state.cart,action.payload]}
+            break;
+        case actions.CHANGE_CART_VISIBILITY:
+            if (state.cartVisibility === false) {
+                newState= {...state,cartVisibility:true}
+            }else {
+                newState= {...state,cartVisibility:false}
+            }
             break;
         default:
             break;
